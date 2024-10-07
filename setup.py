09d03e6aab8e52cb75e10bb04c2d1c00fe3aecd2
whoisamyy <egegegg002@gmail.com>
2024-10-07 18:40:38 +0300

    publishing things

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='fruitspace.py',
+    version='1.0',
+    description='SDK for working with FruitSpace\'s databases',
+    author='whoisamyy',
+    author_email='egegegg002@gmail.com',
+    packages=['fruitspace'],
+    install_requires=['requests','setuptools'],
+    package_dir={'': 'src'},
+)
