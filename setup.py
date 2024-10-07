6c5e94c8aa4e0d0d75d83f6d8c844a2cdc900325
whoisamyy <127599132+whoisamyy@users.noreply.github.com>
2024-10-07 19:21:50 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='fruitspace.py',
-    version='1.0',
+    version='1.0.1',
     description='SDK for working with FruitSpace\'s databases',
     author='whoisamyy',
     author_email='egegegg002@gmail.com',
